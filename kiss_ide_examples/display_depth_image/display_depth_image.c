c60d4023bcff7fa64c408c2e2525fc57be70f6e2
Stefan Zeltner <zeltner@usc.edu>
2013-11-15 18:27:07 -0800

    Add the empty files of a 'Display Depth Image' Example

---
+++ kiss_ide_examples/display_depth_image/display_depth_image.c
@@ -0,0 +1,35 @@
+/* *****************************************************************************
+
+    This file is part of the depth sensor library for the
+        KIPR Link Robot Controller.
+
+    libkipr_link_depth_sensor is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    libkipr_link_depth_sensor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with libkipr_link_depth_sensor.
+    If not, see <http://www.gnu.org/licenses/>.
+
+*******************************************************************************/
+
+int main(int argc, char** argv)
+{
+  if(!depth_open())
+  {
+    printf("Unable to open libkipr_link_depth_sensor\n");
+    return 1;
+  }
+  
+  /* not implemented yet */
+  
+  depth_close();
+
+  return 0;
+}
