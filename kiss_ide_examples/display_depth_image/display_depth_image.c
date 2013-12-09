e3047d132f996316e7b0e8e1f59e04a85984f4ba
Stefan Zeltner <zeltner@usc.edu>
2013-12-09 15:13:48 -0800

    Improve example
    * Print current depth under the mouse pointer
    * Display depth varying the HSV hue instead of the RGB greyscale.

--- kiss_ide_examples/display_depth_image/display_depth_image.c
+++ kiss_ide_examples/display_depth_image/display_depth_image.c
@@ -19,6 +19,60 @@
 
 *******************************************************************************/
 
+#include <stdlib.h>
+
+#define SHOW_DEPTH_UNDER_MOUSE
+
+void HSVtoRGB(int hue, int* r, int* g, int* b)
+{
+  // http://en.wikipedia.org/wiki/HSV_color_space#From_HSV
+  // s = 1.0
+  // v = 1.0
+  // c = 1.0
+  // m = 0
+
+  int h = hue/60;
+
+  int x = 0xFF - abs(h % (2*0xFF) - 0xFF);
+
+  if(h < (1*0xFF))
+  {
+    *r = 0xFF;
+    *g = x;
+    *b = 0;
+  }
+  else if(h < (2*0xFF))
+  {
+    *r = x;
+    *g = 0xFF;
+    *b = 0;
+  }
+  else if(h < (3*0xFF))
+  {
+    *r = 0;
+    *g = 0xFF;
+    *b = x;
+  }
+  else if(h < (4*0xFF))
+  {
+    *r = 0;
+    *g = x;
+    *b = 0xFF;
+  }
+  else if(h < (5*0xFF))
+  {
+    *r = x;
+    *g = 0;
+    *b = 0xFF;
+  }
+  else
+  {
+    *r = 0xFF;
+    *g = 0;
+    *b = x;
+  }
+}
+
 int main(int argc, char** argv)
 {
   if(!depth_open())
@@ -26,18 +80,24 @@ int main(int argc, char** argv)
     printf("Unable to open libkipr_link_depth_sensor\n");
     return 1;
   }
-  
+
   int depth_image_height = -1;
   int depth_image_width = -1;
-  
+
+  int mouse_x;
+  int mouse_y;
+
+  int last_max_depth = 6000 /* mm */;
+  int max_depth;
+
   if(set_depth_camera_resolution(DEPTH_CAMERA_RESOLUTION_640_480) == 0)
   {
     printf("Failed to set the depth camera resolution to 640 x 480\n");
     return 1;
   }
-  
+
   printf("Press 'Q' to stop\n");
-  
+
   while(!get_key_state('Q'))
   {
     if(depth_update())
@@ -47,37 +107,55 @@ int main(int argc, char** argv)
         // initialize the graphics output
         depth_image_height = depth_image_get_height();
         depth_image_width = depth_image_get_width();
-        
+
         graphics_open(depth_image_width, depth_image_height);
       }
-      
+
+      get_mouse_position(&mouse_x, &mouse_y);
+      max_depth = 0;
+
       // display depth image
       for(int y = 0; y < depth_image_height; y++)
       {
         for(int x = 0; x < depth_image_width; x++)
         {
           int depth = get_depth_value(x, y);
-          
-          // max depth is 2 meter
-          if(depth > 2000)
+
+          // save max depth
+          if(depth > max_depth)
           {
-            depth = 2000;
+            max_depth = depth;
           }
-          
+
           // color invalid depth pixel red
           if(depth)
           {
-            int color = 0xFF*depth/2000;
-            graphics_pixel(x, y, color, color, color);
+            int r;
+            int g;
+            int b;
+
+            HSVtoRGB(360*0xFF*depth/last_max_depth, &r, &g, &b);
+
+            graphics_pixel(x, y, r, g, b);
           }
           else
           {
-            graphics_pixel(x, y, 0xFF, 0x00, 0x00);
+            graphics_pixel(x, y, 0xFF, 0xFF, 0xFF);
           }
+
+          #ifdef SHOW_DEPTH_UNDER_MOUSE
+            if(mouse_x == x && mouse_y == y)
+            {
+              console_clear();
+              printf("depth at (%d, %d) is %d mm\n", x, y, depth);
+            }
+          #endif
         }
       }
-	  
-	  graphics_update();
+
+      last_max_depth = max_depth;
+
+      graphics_update();
     }
     else
     {
@@ -85,7 +163,7 @@ int main(int argc, char** argv)
       msleep(2000);
     }
   }
-  
+
   graphics_close();
   depth_close();
 
